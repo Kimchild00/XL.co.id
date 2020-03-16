4209dc165bb551fadbaa3874b2e4bcb03a3d2689
wehmam <readytosurff@gmail.com>
2020-03-16 16:36:28 +0700

    Footer dan navbar selesai

--- index.php
+++ index.php
@@ -3,28 +3,69 @@ include 'navbar.php';
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
+  <!-- footer -->
 
 
+  <!-- Optional JavaScript -->
+  <div class="container-fluid" style="background-color:blue; height:100vh">
+    <div class="row">
 
-<!-- footer -->
+      <div class="col-md-4">
+        <div class="row mt-5 mx-1">
+          <div class="col-md-10"  style="border-right:1px solid grey;">
+            <img src="img_navbar_footer/logo2.png" class="img-flui w-50" alt="">
+            <h4 class="mt-4 text-light">PT. XL Axiata Tbk</h4>
+            <p class="mt-3 text-light">XL Axiata Tower<br>
+              JL. H. R. Rasuna Said X5 Kav. 11-12 Kuningan Timur, Setiabudi, Jakarta Selatan 12950 – Indonesia<br>
+            T. 817 (khusus nomor XL)<br>
+            T. 021 - 57959817 (semua operator)<br>
+            E. customerservice@xl.co.id
+            </p>
+          </div>
+        </div>
+      </div>
+          <br>
+          <div class="col-md-4 mt-5" style="border-right:1px solid grey;"><br><br>
+            <h5 class="mt-5 text-light ml-3" style="font-weight:bold;">Ikuti Kami :</h5>
+            <img src="img_navbar_footer/youtube.png" class="img-fluid" alt="">
+            <img src="img_navbar_footer/twitter.png" class="img-fluid" alt="">
+            <img src="img_navbar_footer/facebook.png" class="img-fluid" alt="">
+            <img src="img_navbar_footer/insta.png" class="img-fluid" alt="">
+          </div>
 
+          <div class="col-md-4 mt-5"><br><br>
+          <h5 class="mt-5 text-light mb-4">Quick Links</h5>
+            <p><a href="#" class="text-light ml-3">XL-Center</a></p>
+            <p><a href="#" class="text-light ml-3">Help</a></p>
+          </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    </div>
+  </div>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  </script>
+</body>
+
 </html>
